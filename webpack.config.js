1196990e56e8c913e3d858d7004ce5ad95baf445
nasustim <mitsuhibino@gmail.com>
2020-09-17 10:32:03 +0900

    webpack.config.jsを整形

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const HTMLPlugin = require("html-webpack-plugin")
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'docs')
+    path: path.resolve(__dirname, 'docs'),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: {
-          loader: "ts-loader",
+          loader: 'ts-loader',
           options: {
             transpileOnly: true,
           },
@@ -19,24 +19,24 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx", ".json"],
+    extensions: ['.js', '.ts', '.tsx', '.json'],
   },
   plugins: [
     new HTMLPlugin({
-      template: path.join(__dirname, "src/index.html"),
+      template: path.join(__dirname, 'src/index.html'),
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'src/voices', to: 'voices' }
-      ]
-    })
+        { from: 'src/voices', to: 'voices' },
+      ],
+    }),
   ],
   externals: {
-    BufferLoader: 'BufferLoader'
-  }
+    BufferLoader: 'BufferLoader',
+  },
 };
